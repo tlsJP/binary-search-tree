81cb791c8b8011473eea5f3320a2b8bd8491bb06
John Albert <jp@thejpaproject.com>
2017-03-01 20:43:14 -0800

    done with depth first

--- src/main/java/com/jp/bst/dfs/DepthFirstSearch.java
+++ src/main/java/com/jp/bst/dfs/DepthFirstSearch.java
@@ -1,29 +1,30 @@
-package com.jp.bst.bfs;
+package com.jp.bst.dfs;
 
 import com.jp.bst.BinaryTree;
 import com.jp.bst.TreeNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
-import java.util.concurrent.ArrayBlockingQueue;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.EmptyStackException;
+import java.util.Stack;
 import java.util.stream.Collectors;
 
 /**
- * Recursive implementation for breadth-first
- * <p>
- * Created by JP on 2/28/2017.
+ * Recursive DFS
+ *
+ * Created by JP on 3/1/2017.
  */
-public class BreadthFirstSearch {
+public class DepthFirstSearch {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(BreadthFirstSearch.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(DepthFirstSearch.class);
 
+    private Stack<TreeNode> uncheckedNodes = new Stack<>();
     private Collection<TreeNode> visitedNodes = new ArrayList<>();
-    private Queue<TreeNode> uncheckedNodes = new ArrayBlockingQueue<>(100);
 
     private TreeNode doSearch(TreeNode root, Comparable target) {
 
-        // We're here now, so we've visited
         visitedNodes.add(root);
 
         Comparable currentValue = root.getValue();
@@ -32,32 +33,37 @@ public class BreadthFirstSearch {
             return root;
         }
 
+        // Drop the kids off at the pool
         if (root.getLeft() != null) {
             uncheckedNodes.add(root.getLeft());
         }
-
         if (root.getRight() != null) {
             uncheckedNodes.add(root.getRight());
         }
 
-        LOGGER.info("Current Queue : [" + uncheckedNodes.stream().map(Object::toString).collect(Collectors.joining(",")) + "]");
+        LOGGER.info("Current stack : Bottom [" + uncheckedNodes.stream().map(Object::toString).collect(Collectors.joining(",")) + ") Top");
 
-        // Time to pop off!
-        TreeNode checkNext = uncheckedNodes.poll();
-        return checkNext == null ? null : doSearch(checkNext, target);
+        TreeNode nextNode = null;
+        try {
+            nextNode = uncheckedNodes.pop();
+        } catch (EmptyStackException e) {
+            LOGGER.info("The stack has been emptied!");
+        }
 
+        return nextNode == null ? null : doSearch(nextNode, target);
     }
 
     public TreeNode search(BinaryTree tree, Comparable target) {
-        LOGGER.info("search({})",target);
+        LOGGER.info("search({})", target);
+
+        // Hmm yeah don't forget to clear these...
         uncheckedNodes.clear();
         visitedNodes.clear();
 
         TreeNode result = doSearch(tree.getRoot(), target);
 
-        // Summary of what happaned
         LOGGER.info("Traversal breadcrumb : " + visitedNodes.stream().map(Object::toString).collect(Collectors.joining(" > ")));
-        LOGGER.info("Remaining unchecked nodes : [" + uncheckedNodes.stream().map(Object::toString).collect(Collectors.joining(",")) + "]");
+        LOGGER.info("Remaining unchecked nodes : Bottom [" + uncheckedNodes.stream().map(Object::toString).collect(Collectors.joining(",")) + ") Top");
 
         return result;
     }
